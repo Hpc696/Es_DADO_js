60bec7ce009ecb51fd1efc7125c57494007da414
Matteo_G <honrop.giochi@gmail.com>
2022-09-16 15:58:10 +0200

    versione non ufficiale ancora soddisfare condizione num intero di facce

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 window.console.log("Inizia il lancio del dado");
-let numfacce= prompt("Inserisci il numero di facce del dado")
+let numfacce= prompt("Inserisci il numero di facce del dado");
 let risultato=0;
+if(numfacce>0 && ){
 for(i=0; i<numfacce; i++){
-    risultato = Math.round(Math.random()*i);
-    i++;
+    risultato = Math.round(Math.random()*numfacce+1);
 }
-document.write("E uscito il cazzo di numero" + "\n" + risultato);
+}
+document.write("E uscito il cazzo di numero" + "<br>" + risultato );
